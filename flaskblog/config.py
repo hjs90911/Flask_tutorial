c2a5dc3a1794118a7469a49dcb9cf645ce6dbfef
Jinsoo Han <hjs90911@naver.com>
2021-07-10 08:34:50 +0900

    Part 11

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = 'da11e6162087007763d61350428690f8'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
